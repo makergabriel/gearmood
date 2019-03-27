c38dfd6d2eace2b704c2ab2ee5c8785b205d5e0d
Bobby Gabriel <bobbygabriel@Bobbys-MacBook-Pro-2.local>
2019-03-26 19:31:07 -0500

    black formatting

--- python_scripts/src/sentence_nlp.py
+++ python_scripts/src/sentence_nlp.py
@@ -5,22 +5,36 @@ from nltk.corpus import wordnet
 import env_config
 
 # TODO move cut words to config
-cut_words = ('drop', 'cut', 'leave', 'lose', 'trim', 'shed', 'cast', 'unload',
-             'strike', 'skip', 'throw', 'shake', 'shave', 'ditch')
-#phrases to include
-#shave some weight
+cut_words = (
+    "drop",
+    "cut",
+    "leave",
+    "lose",
+    "trim",
+    "shed",
+    "cast",
+    "unload",
+    "strike",
+    "skip",
+    "throw",
+    "shake",
+    "shave",
+    "ditch",
+)
+# phrases to include
+# shave some weight
 
-#phrases to ignore
-#leave no trace
+# phrases to ignore
+# leave no trace
 
 # A verb could be categorized to any of the following codes
 VERB_CODES = {
-    'VB',  # Verb, base form
-    'VBD',  # Verb, past tense
-    'VBG',  # Verb, gerund or present participle
-    'VBN',  # Verb, past participle
-    'VBP',  # Verb, non-3rd person singular present
-    'VBZ',  # Verb, 3rd person singular present
+    "VB",  # Verb, base form
+    "VBD",  # Verb, past tense
+    "VBG",  # Verb, gerund or present participle
+    "VBN",  # Verb, past participle
+    "VBP",  # Verb, non-3rd person singular present
+    "VBZ",  # Verb, 3rd person singular present
 }
 
 
@@ -29,41 +43,39 @@ class Sentence:
         config = env_config.EnvConfig()
 
     def parse_candidate_text(self, text, sentence):
-        parsed_text = {
-            "sentence" : sentence
-        }
+        parsed_text = {"sentence": sentence}
         cut_word_objs = []
-        #take a closer look at which cut word is used in the sentence and how it's used
+        # take a closer look at which cut word is used in the sentence and how it's used
         for cut_word in cut_words:
-            if (cut_word in text):
-                #check if the cut_word is used as a verb
+            if cut_word in text:
+                # check if the cut_word is used as a verb
                 result = nltk.pos_tag(text)
                 cut_word_obj = {
-                    "value" : cut_word,
-                    "pos_tag" :[word_obj[1] for word_obj in result if word_obj[0] == cut_word]
+                    "value": cut_word,
+                    "pos_tag": [word_obj[1] for word_obj in result if word_obj[0] == cut_word],
                 }
                 cut_word_objs.append(cut_word_obj)
         parsed_text["words"] = cut_word_objs
         return parsed_text
-    
+
     # refactor to attach the sentence to the comment id
     def parse_comments(self, comments_raw):
-        #sent_toknenize doesn't stop at new lines/carriage returns
-        #some comments use just the phrase with a punctuaion
-        paragraphs = [p for p in comments_raw.split('\n') if p]
+        # sent_toknenize doesn't stop at new lines/carriage returns
+        # some comments use just the phrase with a punctuaion
+        paragraphs = [p for p in comments_raw.split("\n") if p]
         sentences = []
         for paragraph in paragraphs:
             sentences.extend(sent_tokenize(paragraph))
-        
+
         cut_sentences = []
-        #lets look for our cut words in the tokenized senteneces
+        # lets look for our cut words in the tokenized senteneces
         for sentence in sentences:
-            #get the words from the sentence to avoid partial matches
+            # get the words from the sentence to avoid partial matches
             tokens = word_tokenize(sentence)
             tokens = [w.lower() for w in tokens]
-            #get the text from the tokenized words
+            # get the text from the tokenized words
             text = nltk.Text(tokens)
-            #only dive into tokenized senntences that have any of our cut words
+            # only dive into tokenized senntences that have any of our cut words
             if any(x in text for x in cut_words):
                 parsed_text = self.parse_candidate_text(text, sentence)
                 cut_sentences.append(parsed_text)
--- python_scripts/src/shakedown_handler.py
+++ python_scripts/src/shakedown_handler.py
@@ -7,6 +7,7 @@ import sentence_nlp
 REPLACE_NO_SPACE = re.compile(r"(\;)|(\:)|(\!)|(\')|(\?)|(\,)|(\")|(\’)|(\*)")
 REPLACE_WITH_SPACE = re.compile(r"(<br\s*/><br\s*/>)|(\-)|(\[)|(\])|(\/)|(\.)|(\()|(\))|(\n)")
 
+
 def preprocess_text(text):
     text = REPLACE_NO_SPACE.sub("", text.lower())
     text = REPLACE_WITH_SPACE.sub(" ", text)
@@ -14,96 +15,105 @@ def preprocess_text(text):
 
 
 class Shakedown:
-	def __init__(self):
-		config = env_config.EnvConfig()
-		self.sentence = sentence_nlp.Sentence()
-		#connect to reddit
-		#this requires some setup on reddit to use the apis
-		#TODO write up in README.md
-		self.reddit = praw.Reddit(config.get_value("Reddit","site_name"), user_agent=config.get_value("Reddit","user_agent"))
-		self.ultralight = self.reddit.subreddit(config.get_value("Reddit","subreddit"))
-		self.gearmood_ps = gearmood_postgresql.GearMoodPS()
- 
-	#TODO extract to handler class
-	#Find shakedown requests on r/ultralight using the flair
-	def get_shakedown_reqs(self, req_limit, comment_limit, save_enabled):
-		for submission in self.ultralight.search(query="flair:Shakedown",limit=req_limit):
-			self.get_and_save_shakedown(submission, comment_limit, save_enabled)
+    def __init__(self):
+        config = env_config.EnvConfig()
+        self.sentence = sentence_nlp.Sentence()
+        # connect to reddit
+        # this requires some setup on reddit to use the apis
+        # TODO write up in README.md
+        self.reddit = praw.Reddit(
+            config.get_value("Reddit", "site_name"),
+            user_agent=config.get_value("Reddit", "user_agent"),
+        )
+        self.ultralight = self.reddit.subreddit(config.get_value("Reddit", "subreddit"))
+        self.gearmood_ps = gearmood_postgresql.GearMoodPS()
 
-	def get_and_save_shakedown(self, submission, comment_limit, save_enabled):
-		shakedown = self.get_shakedown_data(submission, comment_limit)
-		if save_enabled:
-				self.gearmood_ps.save_shakedown(shakedown)
-				print("Saved shakedown request {} : {}".format(shakedown["title"], "https://www.reddit.com/" + shakedown["id"]))
-				self.gearmood_ps.save_sentence(shakedown["id"], shakedown["cut_sentences"])
+    # TODO extract to handler class
+    # Find shakedown requests on r/ultralight using the flair
+    def get_shakedown_reqs(self, req_limit, comment_limit, save_enabled):
+        for submission in self.ultralight.search(query="flair:Shakedown", limit=req_limit):
+            self.get_and_save_shakedown(submission, comment_limit, save_enabled)
 
-	# Gather other data contained on the reddit posts and store in a collection for further parsing
-	def get_shakedown_data(self, submission, comment_limit):
-		links = re.findall("https?://[^\\s|\\]|\\)]+", submission.selftext)
-		
-		non_negotiable_items_clean = self.clean_non_negotiable_items(submission)
-		
-		# process comments
-		comments_raw, comments_clean, comments_links = self.extract_comments(submission, comment_limit)
+    def get_and_save_shakedown(self, submission, comment_limit, save_enabled):
+        shakedown = self.get_shakedown_data(submission, comment_limit)
+        if save_enabled:
+            self.gearmood_ps.save_shakedown(shakedown)
+            print(
+                "Saved shakedown request {} : {}".format(
+                    shakedown["title"], "https://www.reddit.com/" + shakedown["id"]
+                )
+            )
+            self.gearmood_ps.save_sentence(shakedown["id"], shakedown["cut_sentences"])
 
-		cut_sentences = self.sentence.parse_comments(comments_raw)
-		print(cut_sentences)
+    # Gather other data contained on the reddit posts and store in a collection for further parsing
+    def get_shakedown_data(self, submission, comment_limit):
+        links = re.findall("https?://[^\\s|\\]|\\)]+", submission.selftext)
 
-		shake = {
-			"subreddit_id" : submission.subreddit_id,
-			"id" : submission.id,
-			"title" : submission.title,
-			"shakedown_links" : links,
-			"non_negotiable_items" : non_negotiable_items_clean,
-			"selftext" : submission.selftext,
-			"comments_clean" : comments_clean,
-			"comments_links" : comments_links,
-			"comments_raw" : comments_raw,
-			"cut_sentences" : cut_sentences,
-			"created_dt" : submission.created_utc
-		}
-		return shake
+        non_negotiable_items_clean = self.clean_non_negotiable_items(submission)
 
-	def clean_non_negotiable_items(self, submission):
-		non_negotiable_items = re.findall("non-?negotiable\\s?items?:?\\s*(.*)\n", submission.selftext.lower())
-		non_negotiable_items_clean = ""
-		for nni in non_negotiable_items:
-				non_negotiable_items_clean += preprocess_text(nni)
-		return non_negotiable_items_clean
+        # process comments
+        comments_raw, comments_clean, comments_links = self.extract_comments(
+            submission, comment_limit
+        )
 
-	# TODO Preserve the comment id so it can be attached to the sentence later
-	# TODO refactor to get the sentences from each comment
-	# TODO get the comment update_dt to know if we already processed it
-	def extract_comments(self, submission, comment_limit):
-		submission.comments.replace_more(limit=comment_limit)
-		comments = submission.comments.list()
-		comments_clean = ""
-		comments_links = []
-		comments_raw = ""
-		for comment in comments:
-			comment_links = re.findall("https?://[^\\s|\\]|\\)]+", comment.body)
-			if comment_links:
-				for comment_link in comment_links:
-					comments_links.append(comment_link)
-					link_pattern = re.compile("(https?://[^\\s|\\]|\\)]+)")
-					comment_body_no_links = link_pattern.sub("", comment.body)
-			else:
-				comment_body_no_links = comment.body
-			comments_clean += preprocess_text(comment_body_no_links)
-			comments_raw += comment.body
-		return comments_raw, comments_clean, comments_links
-	
-	def get_shakedown_req_by_id(self, submission_id):
-		submission = self.reddit.submission(id=submission_id)
-		self.get_and_save_shakedown(submission, 100, True)
+        cut_sentences = self.sentence.parse_comments(comments_raw)
+        print(cut_sentences)
 
-if __name__ == '__main__':
-	shakedown = Shakedown()
-	#TODO move the limits to command line arguments
-	#TODO maintain starting point
+        shake = {
+            "subreddit_id": submission.subreddit_id,
+            "id": submission.id,
+            "title": submission.title,
+            "shakedown_links": links,
+            "non_negotiable_items": non_negotiable_items_clean,
+            "selftext": submission.selftext,
+            "comments_clean": comments_clean,
+            "comments_links": comments_links,
+            "comments_raw": comments_raw,
+            "cut_sentences": cut_sentences,
+            "created_dt": submission.created_utc,
+        }
+        return shake
 
-	shakedown.get_shakedown_reqs(500, 100, True)
-	#shakedown.get_shakedown_req_by_id("ay4ana")
+    def clean_non_negotiable_items(self, submission):
+        non_negotiable_items = re.findall(
+            "non-?negotiable\\s?items?:?\\s*(.*)\n", submission.selftext.lower()
+        )
+        non_negotiable_items_clean = ""
+        for nni in non_negotiable_items:
+            non_negotiable_items_clean += preprocess_text(nni)
+        return non_negotiable_items_clean
+
+    # TODO Preserve the comment id so it can be attached to the sentence later
+    # TODO refactor to get the sentences from each comment
+    # TODO get the comment update_dt to know if we already processed it
+    def extract_comments(self, submission, comment_limit):
+        submission.comments.replace_more(limit=comment_limit)
+        comments = submission.comments.list()
+        comments_clean = ""
+        comments_links = []
+        comments_raw = ""
+        for comment in comments:
+            comment_links = re.findall("https?://[^\\s|\\]|\\)]+", comment.body)
+            if comment_links:
+                for comment_link in comment_links:
+                    comments_links.append(comment_link)
+                    link_pattern = re.compile("(https?://[^\\s|\\]|\\)]+)")
+                    comment_body_no_links = link_pattern.sub("", comment.body)
+            else:
+                comment_body_no_links = comment.body
+            comments_clean += preprocess_text(comment_body_no_links)
+            comments_raw += comment.body
+        return comments_raw, comments_clean, comments_links
+
+    def get_shakedown_req_by_id(self, submission_id):
+        submission = self.reddit.submission(id=submission_id)
+        self.get_and_save_shakedown(submission, 100, True)
 
 
+if __name__ == "__main__":
+    shakedown = Shakedown()
+    # TODO move the limits to command line arguments
+    # TODO maintain starting point
 
+    shakedown.get_shakedown_reqs(500, 100, True)
+    # shakedown.get_shakedown_req_by_id("ay4ana")
